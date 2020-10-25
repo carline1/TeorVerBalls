781d4b17948090bb2321a8a5df1e294399ee86d1
carline <ivlad12345678932@gmail.com>
2020-10-25 16:15:00 +0300

    Добавил версию без счетчиков комбинаций

---
+++ TeorVerBalls2_whithout_counters/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeorVerBalls2
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int l1 = 40;  // запоминаем left позицию шара первого бокса, чтобы потом вернуть его обратно
+        int t1 = 34;  // запоминаем top позицию шара  первого бокса, чтобы потом вернуть его обратно
+        int n1 = 0;  // запоминаем номер шара в первом массиве, тобы вернуть ему расположение
+
+        int l2 = 980;  // запоминаем left позицию шара второго бокса, чтобы потом вернуть его обратно
+        int t2 = 34;  // запоминаем top позицию шара  второго бокса, чтобы потом вернуть его обратно
+        int n2 = 0;  // запоминаем номер шара во втором массиве, тобы вернуть ему расположение
+
+        int tests = 0;  // кол-во испытаний
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tests++;
+            label4.Text = "Всего испытаний: " + tests;
+
+            PictureBox[] images1 = new PictureBox[9]
+            {
+                pictureBox1,
+                pictureBox2,
+                pictureBox3,
+                pictureBox4,
+                pictureBox5,
+                pictureBox6,
+                pictureBox7,
+                pictureBox8,
+                pictureBox9,
+            };
+            // возращаем прошлый шар на место
+            images1[n1].Left = l1;  
+            images1[n1].Top = t1;  
+
+            // рандомим следующий шар и запоминаем его исходную позицию
+            n1 = rnd.Next(0, 8);
+            l1 = images1[n1].Left;
+            t1 = images1[n1].Top;
+            images1[n1].Left = 479;
+            images1[n1].Top = 407;
+
+
+            PictureBox[] images2 = new PictureBox[9]
+            {
+                pictureBox10,
+                pictureBox11,
+                pictureBox12,
+                pictureBox13,
+                pictureBox14,
+                pictureBox15,
+                pictureBox16,
+                pictureBox17,
+                pictureBox18,
+            };
+            images2[n2].Left = l2;
+            images2[n2].Top = t2;
+
+            n2 = rnd.Next(0, 8);
+            l2 = images2[n2].Left;
+            t2 = images2[n2].Top;
+            images2[n2].Left = 585;
+            images2[n2].Top = 407;
+        }
+    }
+}
